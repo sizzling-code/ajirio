35d4ae7e97e39d18cc878e3d97ad9cf43cd3835d
romanusbett <bettromanus@gmail.com>
2025-11-11 10:14:45 +0300

    add auth form component

--- ajirioClient/src/app/components/ajirio-auth-form/ajirio-auth-form.component.ts
+++ ajirioClient/src/app/components/ajirio-auth-form/ajirio-auth-form.component.ts
@@ -1,15 +1,21 @@
-import { AjirioFormInputComponent } from './../ajirio-form-input/ajirio-form-input.component';
 import { Component } from '@angular/core';
-
+import { input } from '@angular/core';
 
 @Component({
   selector: 'app-ajirio-auth-form',
-  imports: [AjirioFormInputComponent],
+  imports: [],
   templateUrl: './ajirio-auth-form.component.html',
   styleUrl: './ajirio-auth-form.component.css'
 })
 export class AjirioAuthFormComponent {
   userRole = 'Seeker';
+
+  imageUrl = input<string>();
+  authPageTitle = input<string>();
+  authPageTagLine = input<string>();
+  authButtonText = input<string>();
+  pageRedirectText = input<string>();
+  redirectLink = input<string>();
 
   setRole(el: string) {
     this.userRole = el;
